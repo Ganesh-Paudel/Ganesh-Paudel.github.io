4f85501ebc6946f3b22084d8305a6af71eb10009
Ganesh-Paudel <paudelganesh080@gmail.com>
2026-03-12 11:50:48 -0400

    Changed the UI, added scripts for Projects to import from json

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿async function loadProjects() {
+  try {
+    const response = await fetch("./projects.json");
+    const projects = await response.json();
+    console.log(projects);
+    const container = document.getElementById("project-container");
+    container.innerHTML = "";
+
+    projects.forEach(project => {
+      const card = document.createElement("div");
+      card.className = "project-card";
+
+      card.innerHTML = `
+        <h3>${project.title}</h3>
+        <p>${project.description}</p>
+        <a href="${project.link}" target="_blank">View Project</a>
+      `;
+
+      container.appendChild(card);
+    });
+  } catch (error) {
+    console.error("Error loading projects:", error);
+  }
+}
+
+loadProjects();
